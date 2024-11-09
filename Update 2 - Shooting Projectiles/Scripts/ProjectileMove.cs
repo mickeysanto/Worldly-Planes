18de4b28336a22b346b22d8de3650ee0ddd14c50
Mitchell Santomartino <mickey.santomartino@gmail.com>
2024-11-09 15:47:26 -0500

    Bug Fix

--- Update 2 - Shooting Projectiles/Scripts/ProjectileMove.cs
+++ Update 2 - Shooting Projectiles/Scripts/ProjectileMove.cs
@@ -8,7 +8,7 @@ public class ProjectileMove : MonoBehaviour
     public ParticleSystem impact = null; // the particle system that plays on impact or max range
 
     public float speed = 2f; // projectile move speed
-    public float sphereRadius = .25f; // radius of the sphere cast
+    public float sphereRadius = .28f; // radius of the sphere cast
     public float range = 10f; // how far the projectile moves before its deactivated
 
     private Vector3 direction;
@@ -28,10 +28,9 @@ public class ProjectileMove : MonoBehaviour
         if(!projectileEnd)
         {
             direction = transform.forward;
-            transform.position += direction * speed * Time.deltaTime;
 
             // if the projectile has gotten to its max range or hit something
-            if (Physics.SphereCast(transform.position, sphereRadius, direction, out hit, speed * Time.deltaTime)
+            if (Physics.SphereCast(transform.position, sphereRadius, direction, out hit, speed * Time.deltaTime * 1.1f)
                 || Vector3.Distance(startingPoint, transform.position) >= range)
             {
                 projectileEnd = true;
@@ -47,6 +46,11 @@ public class ProjectileMove : MonoBehaviour
                 {
                     gameObject.SetActive(false);
                 }
+            }
+            else
+            {
+                // moves projectile
+                transform.position += direction * speed * Time.deltaTime;
             }
         }
     }
